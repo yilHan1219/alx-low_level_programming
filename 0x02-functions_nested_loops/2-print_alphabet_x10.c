15f5cc689188082daee1a71060e65efd7d488eb3
yilHan1219 <hanyohan87@gmail.com>
2022-06-10 02:51:31 -0700

    alphabet in lower case

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,19 @@
 # include "main.h"
 
 /**
- * print _alphabet_x10 - Check code
- * Description: function uses _putchar function to print
- * alphabet in lowercase 10 times
- * Return: Nothing
+ * print_alphabet_X10 - Check description
+ * Description: It prints the alphabet  10 times in lowercase followed by a new line
+ * Return: Nothing.
  */
+
 
 void print_alphabet_x10(void)
 {
 char i;
 int j;
-for (j= 0; j< 10; j++)
+for (j = 0; j < 10; j++)
 {
-for (i= 'a'; i<= 'z'; i++)
+for (i = 'a'; i <= 'z'; i++)
 _putchar(i);
 _putchar('\n');
 }
