9e888ed1183dadefacffa258377736d866926145
yilHan1219 <hanyohan87@gmail.com>
2022-06-10 06:03:43 -0700

    intiger number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+void print_integer(int m);
+
+/**
+ * print_number - a function that prints an integer.
+ * @n: An input integer
+ * Return: Nothing
+ */
+void print_number(int n)
+{
+if (n == 0)
+_putchar('0');
+else if (n < 0)
+{
+_putchar('-');
+print_integer(n * -1);
+}
+else
+print_integer(n);
+}
