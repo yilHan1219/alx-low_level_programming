8bf6a7414377796e6d3c26bdddca10efc721e22b
yilHan1219 <hanyohan87@gmail.com>
2022-06-15 12:57:44 -0700

    Print integers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+void print_number(int n)
+{
+int power;
+
+power = base10(8);
+
+if (n < 0)
+{
+_putchar('-');
+n *= -1;
+}
+
+if (n == 0)
+_putchar('0');
+
+else
+{
+while (n / power == 0)
+power /= 10;
+
+while (power >= 1)
+{
+_putchar((n / power) + '0');
+n %= power;
+power /= 10;
+}
+}
+}
